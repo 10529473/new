a1aabb7d08e039393f08e4d618de57d5c381d27c
Willian de Paula Araujo <10529473@mydbs.ie>
2020-02-17 10:28:05 +0000

    print lowest student no

--- students.py
+++ students.py
@@ -27,6 +27,16 @@ students=[]
 def add(Student):
     students.append(Student)
 
+def retrieve():
+    lowestNumber = "99999999"
+    for student in students:
+        if student.getStudentNumber() <  lowestNumber:
+            lowestNumber = student.getStudentNumber()
+    print(lowestNumber)
 #%%
 
 add(Student("12345678","John Doe","1234567"))
+add(Student("00000001","John Doe","1234567"))
+add(Student("00000010","Will","1234567"))
+
+retrieve()
